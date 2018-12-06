c9914e990fb756ef3406eae324844ef0d7be971f
Chris <christocarr@outlook.com>
2018-12-06 23:25:01 +0000

    Add form container to button on click functionality

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 window.onload = function() {
   //get button to show more categories
-  const EXTRA_CATEGORIES = document.getElementById('extraCategories');
+  const EXTRA_CATEGORIES_BUTTON = document.getElementById('extraCategories');
   //get div holding extra categories
   const EXTRA_CAT_CONTAINER = document.getElementById('extraCatContainer');
+  //get signup container
+  const SIGNUP_CONTAINER = document.getElementById('newsletterSignup');
 
   //when user clicks show more categories button display extra categories and sign-up form
-  EXTRA_CATEGORIES.addEventListener('click', function() {
+  EXTRA_CATEGORIES_BUTTON.addEventListener('click', function() {
     EXTRA_CAT_CONTAINER.style.display = 'block';
-    EXTRA_CATEGORIES.style.display = 'none';
+    EXTRA_CATEGORIES_BUTTON.style.display = 'none';
+    SIGNUP_CONTAINER.style.display = 'block';
   });
 }
